a83473e280a812b96359c956105a3015eacd5b87
MichalPruszynski7 <mpruszynski@hawk.iit.edu>
2024-02-29 23:31:03 -0600

    java

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  function init (){
+	var tcButton = document.getElementById("attentiontext");
+
+  function myevent() {
+	alert("Please dont Be Alarmed This Event is Family Friendly :)");
+  }
+
+  tcButton.addEventListener("click", myEvent);
+  }
+  window.addEventListener('load', init)
